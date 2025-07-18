6589d44f694766b7ce6165db7d8f954b5bed1964
ritik-wo <ritik.webosmotic@gmail.com>
2025-07-18 18:31:46 +0530

    feat - removed hardcoded values

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "2851b584-7025-47ce-ba67-5dd1922ce0ad")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "~TE8Q~RhSAe68ukOaDTEVhTUKKVTQEUzEY1vycxS")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
